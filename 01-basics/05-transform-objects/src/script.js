f5db8c62b0bf2c1487ebf67cb741ef360b24a3cd
Gabriel Borges <gbdesigns13@gmail.com>
2022-05-22 15:11:13 -0300

    Transform objects

---
+++ 01-basics/05-transform-objects/src/script.js
@@ -0,0 +1,70 @@
+import "./style.css";
+import * as THREE from "three";
+
+// Canvas
+const canvas = document.querySelector("canvas.webgl");
+
+// Scene
+const scene = new THREE.Scene();
+
+/**
+ * Objects
+ */
+const group = new THREE.Group();
+
+// group.position.y = 1;
+// group.scale.y = 1;
+// group.rotation.y = 1;
+
+scene.add(group);
+
+const cube1 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 0xff0000 })
+);
+
+const cube2 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 0x00ff00 })
+);
+cube2.position.x = -1.5;
+
+const cube3 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 0x0000ff })
+);
+cube3.position.x = 1.5;
+
+group.add(cube1);
+group.add(cube2);
+group.add(cube3);
+
+// Axes Helper
+const axesHelper = new THREE.AxesHelper();
+scene.add(axesHelper);
+
+/**
+ * Sizes
+ */
+const sizes = {
+  width: 800,
+  height: 600,
+};
+
+/**
+ * Camera
+ */
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
+camera.position.z = 3;
+scene.add(camera);
+
+// camera.lookAt(mesh.position);
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas,
+});
+renderer.setSize(sizes.width, sizes.height);
+renderer.render(scene, camera);
